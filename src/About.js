210f0682ae5c687c326e1265aed5c26510d796cf
Daniel Zheng <danielzheng@Daniels-MacBook-Pro-3.local>
2020-06-18 15:53:31 -0400

    added material ui theme, still need to change defaults

--- src/About.js
+++ src/About.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import styled from 'styled-components';
+import { Typography } from '@material-ui/core';
 
 const About = () => { 
     return ( 
-        <p>about us text</p>
+        <Typography variant="h1" color="primary">What is Offhours</Typography>
     )
 }
  export default About; 
