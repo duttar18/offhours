ebde74e3ebd9906060b338a59c0bc4198028a549
Raul Dutta <duttar@umich.edu>
2020-06-22 12:59:10 -0400

    add acces control origin

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
     return (
       <div>
         <Grid container direction="column"> 
-        <NavigationBar/>
+        
         <Layout>
           <Router>
             <Switch>
