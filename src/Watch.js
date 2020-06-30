4e6d0348a6d9ba1dfd327eb9ddd1b66b76293d9c
Daniel Zheng <danielzheng@Daniels-MacBook-Pro-3.local>
2020-06-30 12:33:41 -0400

    fixed formatting/typos

--- src/Watch.js
+++ src/Watch.js
@@ -1,63 +1,59 @@
-import React, {useState} from 'react'
-import styled from 'styled-components';
-import { TwitchEmbed, TwitchPlayer } from 'react-twitch-embed';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { TwitchEmbed, TwitchPlayer } from "react-twitch-embed";
 import { useLocation } from "react-router-dom";
-var images = require.context('./img', true);
+var images = require.context("./img", true);
 
 const Styles = styled.div`
-  .avatar {
-      border-radius : 50%;
-      width: 70px;
-      height: 70px;
-  }
-  .streamer {
-    margin: 0px 10px 10px 10px;
-    width: 400px;
-    height: 100px;
-    display: flex;
-    justify-content: right;
-    align-items: center;
-  }
-  .streamer p {
-    margin-left: 10px;
-    font-size: 30px;
-  }
+    .avatar {
+        border-radius: 50%;
+        width: 70px;
+        height: 70px;
+    }
+    .streamer {
+        margin: 0px 10px 10px 10px;
+        width: 400px;
+        height: 100px;
+        display: flex;
+        justify-content: right;
+        align-items: center;
+    }
+    .streamer p {
+        margin-left: 10px;
+        font-size: 30px;
+    }
 `;
 
+const Watch = () => {
+    let location = useLocation();
+    const query = new URLSearchParams(location.search);
+    const username = query.get("username");
+    const [live, setLive] = useState(true);
+    const [name, setName] = useState("");
 
-const Watch = () => {    
+    // get stream ; send stream id
+    // what is sent to backend
 
-  let location = useLocation();    
-  const query = new URLSearchParams(location.search);
-  const username = query.get('username')
-  const [live,setLive] = useState(true)
-  const [name,setName] = useState('')
-  
-  // get stream ; send stream id
-  // what is sent to backend
-  
-  fetch('https://off-hours-backend.herokuapp.com/stream?username='+username)
-  .then(response => response.json())
-  .then(data => {
-      setLive(data.isLive)
-      setName(data.name)
-  });
+    fetch("https://off-hours-backend.herokuapp.com/stream?username=" + username)
+        .then((response) => response.json())
+        .then((data) => {
+            setLive(data.isLive);
+            setName(data.name);
+        });
 
-// <TwitchPlayer video= {stream["vidnum"]} />
-  return (
-    <Styles>
-        {live?(
-        <TwitchEmbed channel={username} withChat={true}/>
-        ):(
-          <p>is not live</p>
-        )}
-        <div class='streamer'>
-            <p>{name}</p>
-        </div>
-    </Styles>
-
-      
-  );
-}
+    // <TwitchPlayer video= {stream["vidnum"]} />
+    return (
+        <Styles>
+            {live ? (
+                <TwitchEmbed channel={username} withChat={true} />
+            ) : (
+                <p>is not live</p>
+            )}
+            <div class="streamer">
+                <p>{name}</p>
+            </div>
+        </Styles>
+    );
+};
 
 export default Watch;
--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
-import axios from 'axios'
+import axios from "axios";
 
 let api = axios.create({
     headers: {
-        'Client-ID': hgzp49atoti7g7fzd9v4pkego3i7ae
-    }
-}) 
+        "Client-ID": hgzp49atoti7g7fzd9v4pkego3i7ae,
+    },
+});
 
 export default api;
