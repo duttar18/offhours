2cc407c6051768535c41f1f9fcfcf237954da030
Daniel Zheng <danielzheng@Daniels-MacBook-Pro-3.local>
2020-07-01 07:38:16 -0400

    change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -50,13 +50,13 @@ export const NavigationBar = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-nav">
                     <Nav className="ml-auto" >
-                        <Nav.Item><Nav.Link href="/browse"><Button color='white'>Browse</Button></Nav.Link></Nav.Item>
+                        <Nav.Item><Nav.Link href="/browse"><Button color="inherent">Browse</Button></Nav.Link></Nav.Item>
                         <Nav.Item><Nav.Link href="/stream"><Button>Stream</Button></Nav.Link></Nav.Item>
                         <Nav.Item><Nav.Link href="/about"><Button>About</Button></Nav.Link></Nav.Item>
                         <Nav.Item>
                             {loggedin ?
-                            (<Nav.Link><Button variant='contained' color='primary' onClick={logout}>Logout({username})</Button></Nav.Link>):
-                            (<Nav.Link href="/login"><Button variant='contained' color='primary'>Login</Button></Nav.Link>)}
+                            (<Nav.Link><Button variant='contained' disableElevation color='primary' onClick={logout}>Logout({username})</Button></Nav.Link>):
+                            (<Nav.Link href="/login"><Button variant='contained' color='primary' disableElevation>Login</Button></Nav.Link>)}
                         </Nav.Item>  
                     </Nav>
                 </Navbar.Collapse>
